84dc5034f826ad7d165a4e3fee1b1a7598811a53
hehong <619215399@qq.com>
2020-02-05 21:07:11 +0800

    base

---
+++ src/main/java/com/threadstduty/ThreadstdutyApplication.java
@@ -0,0 +1,13 @@
+package com.threadstduty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadstdutyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadstdutyApplication.class, args);
+    }
+
+}
